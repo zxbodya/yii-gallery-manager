dda589138d6422aa6d181141b32e863d36f8447a
Bogdan Savluk <Savluk.Bogdan@gmail.com>
2012-08-02 04:29:46 +0300

    Added ability for few gallery manager widgets on same page. Moved styles and scripts from view to separate files, added minified script versions.

--- GalleryManager.php
+++ GalleryManager.php
@@ -21,13 +21,36 @@ class GalleryManager extends CWidget
     /** Render widget */
     public function run()
     {
-
+        /** @var $cs CClientScript */
         $cs = Yii::app()->clientScript;
+        $cs->registerCssFile($this->assets . '/galleryManager.css');
+
         $cs->registerCoreScript('jquery');
-        $cs->registerScriptFile($this->assets . '/jquery.iframe-transport.min.js');
+        $cs->registerCoreScript('jquery.ui');
+
+        if (defined('YII_DEBUG')) {
+            $cs->registerScriptFile($this->assets . '/jquery.iframe-transport.js');
+            $cs->registerScriptFile($this->assets . '/jquery.galleryManager.js');
+        } else {
+            $cs->registerScriptFile($this->assets . '/jquery.iframe-transport.min.js');
+            $cs->registerScriptFile($this->assets . '/jquery.galleryManager.min.js');
+        }
 
         if ($this->controllerRoute === null)
             throw new CException('$controllerRoute must be set.', 500);
+
+        $opts = CJavaScript::encode(array(
+            'hasName:' => $this->gallery->name ? true : false,
+            'hasDesc:' => $this->gallery->description ? true : false,
+            'uploadUrl' => Yii::app()->createUrl($this->controllerRoute . '/ajaxUpload', array('gallery_id' => $this->gallery->id)),
+            'deleteUrl' => Yii::app()->createUrl($this->controllerRoute . '/delete'),
+            'updateUrl' => Yii::app()->createUrl($this->controllerRoute . '/changeData'),
+            'arrangeUrl' => Yii::app()->createUrl($this->controllerRoute . '/order'),
+            'nameLabel' => Yii::t('galleryManager.main', 'Name'),
+            'descriptionLabel' => Yii::t('galleryManager.main', 'Description'),
+        ));
+        $src = "$('#{$this->id}').galleryManager({$opts});";
+        $cs->registerScript('galleryManager#' . $this->id, $src);
         $model = new GalleryPhoto();
         $this->render('galleryManager', array(
             'model' => $model,
